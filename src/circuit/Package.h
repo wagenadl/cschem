0b0874ad7f0f1cb5c7d586d7d363245f9317079b
Daniel A. Wagenaar <daw@caltech.edu>
2018-01-30 10:50:20 -0800

    Packaging stuff

--- src/circuit/Package.h
+++ src/circuit/Package.h
@@ -4,27 +4,17 @@
 
 #define PACKAGE_H
 
-#include <QSharedData>
 #include <QXmlStreamReader>
-#include  <QString>
+#include <QString>
 #include <QMap>
 
 class Package {
 public:
-  Package();
-  Package(Package const &);
-  Package &operator=(Package const &);
-  ~Package();
   QString report() const;
 public:
-  QString name() const; // e.g., "r3" or "dip8"
-  QString pcb() const; // e.g., "ACY300" or  "DIP8"
-  QMap<QString, int> pinmap() const;
-private:
-  friend QXmlStreamWriter &operator<<(QXmlStreamWriter &, Package const &);
-  friend QXmlStreamReader &operator>>(QXmlStreamReader &, Package &);
-private:
-  QSharedDataPointer<class PackageData> d;
+  QString name;
+  QString pcb;
+  QMap<QString, int> pinmap;
 };
 
 QXmlStreamWriter &operator<<(QXmlStreamWriter &, Package const &);
