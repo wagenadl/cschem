abeec4e4b4fa54a54e875629f0319a47d0859c92
Daniel A. Wagenaar <daw@caltech.edu>
2018-02-05 15:59:50 -0800

    Working on package panel

--- src/svg/PackageLibrary.h
+++ src/svg/PackageLibrary.h
@@ -13,6 +13,7 @@ public:
   void merge(Packaging const &);
   void setPath(QString);
   class PackageDrawing const &drawing(QString name) const; // our name, not pcb's
+  QStringList compatiblePackages(class Symbol const &) const;
 public:
   static PackageLibrary const &defaultPackages();
   static QString defaultPath();
