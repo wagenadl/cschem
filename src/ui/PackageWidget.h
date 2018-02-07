2dcb030a5249e39d92913f14b1d822773fd1c689
Daniel A. Wagenaar <daw@caltech.edu>
2018-02-07 15:49:51 -0800

    PackagePanel now really looks nice

--- src/ui/PackageWidget.h
+++ src/ui/PackageWidget.h
@@ -12,18 +12,18 @@ public:
   PackageWidget(QWidget *parent=0);
   ~PackageWidget();
   double scale() const; // returns scale in pixels per 0.001". Default is 0.1.
-  bool isFreeScaling() const; // true means: will expand to available space
+  double gridSpacing() const;
+  int gridInterval() const;
+public:
   QSize sizeHint() const override;
   QSize minimumSizeHint() const override;
-  int margin() const; // ideally, determined by grid spacing
-  double gridSpacing() const;
 signals:
   void pressed(QString); // argument is name of package
 public slots:
   void setPackage(QString name);
   void setLibrary(class PackageLibrary const *lib);
   void setScale(double pix_per_mil);
-  void setFreeScaling(bool);
+  void setGrid(double mil, int major=5);
 protected:
   void paintEvent(QPaintEvent *) override;
   void mousePressEvent(QMouseEvent *) override;
