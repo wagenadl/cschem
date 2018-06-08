e69afb305d4e9670b99f15d6b3c2d210a436dd77
Daniel A. Wagenaar <daw@caltech.edu>
2018-06-07 23:26:51 -0400

    Slight improvements to dimspinner

--- src/ui/DimSpinner.cpp
+++ src/ui/DimSpinner.cpp
@@ -3,14 +3,25 @@
 #include "DimSpinner.h"
 
 DimSpinner::DimSpinner(QWidget *parent): QDoubleSpinBox(parent) {
+  metric_ = false;
+  hasvalue_ = true;
+  minv = Dim::fromInch(0.);
+  maxv = Dim::fromInch(100.);
   step = Dim::fromInch(.005);
   setInch();
   connect(this,
     static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
 	  [this](double v) {
+	    if (!hasvalue_ && hasValue()) {
+	      // meaning, we *now* have a value
+	      hasvalue_ = true;
+	      setMinimumValue(minv);
+	    }
 	    valueChanged(metric_ ? Dim::fromMM(v) : Dim::fromInch(v));
 	  });
-	  
+}
+
+DimSpinner::~DimSpinner() {
 }
 
 Dim DimSpinner::value() const {
@@ -28,7 +39,23 @@ bool DimSpinner::isInch() const {
   return !metric_;
 }
 
+void DimSpinner::setNoValue() {
+  hasvalue_ = false;
+  setMinimumValue(minv);
+  QDoubleSpinBox::setSpecialValueText("---");
+  QDoubleSpinBox::setValue(QDoubleSpinBox::minimum());
+}
+
+bool DimSpinner::hasValue() const  {
+  return hasvalue_ ? true : value() > minv - step/2;
+}
+
 void DimSpinner::setValue(Dim d) {
+  if (!hasvalue_) {
+    hasvalue_ = true;
+    QDoubleSpinBox::setSpecialValueText(QString());
+    setMinimumValue(minv);
+  }
   if (metric_) 
     QDoubleSpinBox::setValue(d.toMM());
   else
@@ -37,20 +64,36 @@ void DimSpinner::setValue(Dim d) {
 
 void DimSpinner::setStep(Dim d) {
   step = d;
-  if (metric_) 
-    QDoubleSpinBox::setSingleStep(d.toMM());
+  QDoubleSpinBox::setSingleStep(metric_ ? d.toMM() : d.toInch());
+}
+
+void DimSpinner::setMinimumValue(Dim d) {
+  minv = d;
+  double mv = metric_ ? minv.toMM() : minv.toInch();
+  if (hasvalue_) 
+    QDoubleSpinBox::setMinimum(mv);
   else
-    QDoubleSpinBox::setSingleStep(d.toInch());
+    QDoubleSpinBox::setMinimum(mv - QDoubleSpinBox::singleStep());
+}
+
+void DimSpinner::setMaximumValue(Dim d) {
+  maxv = d;
+  double mv = metric_ ? maxv.toMM() : maxv.toInch();
+  QDoubleSpinBox::setMaximum(mv);
 }
 
 void DimSpinner::setMetric(bool b) {
   if (b) {
+    bool ok = hasValue();
     Dim d = value();
     metric_ = true;
     setSuffix(" mm");
     setDecimals(2);
     setStep(step);
-    setValue(d);
+    setMinimumValue(minv);
+    setMaximumValue(maxv);
+    if (ok)
+      setValue(d);
   } else {
     setInch();
   }
@@ -58,9 +101,23 @@ void DimSpinner::setMetric(bool b) {
 
 void DimSpinner::setInch() {
   Dim d = value();
+  bool ok = hasValue();
   metric_ = false;
   setSuffix("”");
   setDecimals(3);
   setStep(step);
-  setValue(d);
+  setMinimumValue(minv);
+  setMaximumValue(maxv);
+  if (ok)
+    setValue(d);
+}
+
+double DimSpinner::valueFromText(QString const &s) const {
+  qDebug() << "valuefromtext" << s;
+  return QDoubleSpinBox::valueFromText(s);
+}
+
+QString DimSpinner::textFromValue(double d) const {
+  qDebug() << "textfromvalue" << d;
+  return QDoubleSpinBox::textFromValue(d);
 }
--- src/ui/DimSpinner0.h
+++ src/ui/DimSpinner0.h
@@ -11,30 +11,18 @@ class DimSpinner: public QDoubleSpinBox {
   Q_OBJECT;
 public:
   DimSpinner(QWidget *parent=0);
-  virtual ~DimSpinner();
   Dim value() const;
   bool isMetric() const;
   bool isInch() const;
-  bool hasValue() const;
-  Dim minimumValue() const;
-  Dim maximumValue() const;
 public slots:
-  void setNoValue();
   void setValue(Dim);
-  void setMinimumValue(Dim);
-  void setMaximumValue(Dim);
   void setMetric(bool b=true);
   void setInch();
   void setStep(Dim);
 signals:
   void valueChanged(Dim);
 private:
-  double valueFromText(QString const &) const override;
-  QString textFromValue(double) const override;
-private:
-  bool hasvalue_;
   bool metric_;
-  Dim minv, maxv;
   Dim step;
 };
 
