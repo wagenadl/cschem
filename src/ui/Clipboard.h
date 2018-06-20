92ec31fc720d4c442a6ec755356ed134e5eeec24
Daniel A. Wagenaar <daw@caltech.edu>
2018-06-20 15:28:27 -0700

    Clipboard now copies library parts

--- src/ui/Clipboard.h
+++ src/ui/Clipboard.h
@@ -5,15 +5,18 @@
 #define CLIPBOARD_H
 
 #include "circuit/Circuit.h"
+#include "svg/SymbolLibrary.h"
 
 class Clipboard {
 public:
   Clipboard();
   static Clipboard &clipboard();
-  void store(Circuit const &);
+  void store(Circuit const &, SymbolLibrary const &);
   Circuit retrieve() const;
+  SymbolLibrary const &library() const;
 private:
   Circuit circ;
+  SymbolLibrary lib;
 };
 
 #endif
