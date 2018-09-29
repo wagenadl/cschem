ea425a155aeb5f01a70d961fb9af8b14581440e4
Daniel A. Wagenaar <daw@caltech.edu>
2018-09-28 22:25:28 -0700

    FilledPlane edges can now be dragged with shift held

---
+++ cpcb/ui/BoardSizeDialog.cpp
@@ -0,0 +1,36 @@
+// BoardSizeDialog.cpp
+
+#include "BoardSizeDialog.h"
+#include "ui_BoardSizeDialog.h"
+#include "DimSpinner.h"
+#include "data/Layout.h"
+
+BoardSizeDialog::BoardSizeDialog(QWidget *parent): QDialog(parent) {
+  ui = new  Ui_BoardSizeDialog;
+  ui->setupUi(this);
+  connect(ui->shrink, &QToolButton::clicked,
+	  [this]() {
+	    ui->width->setValue(minrect.right());
+	    ui->height->setValue(minrect.bottom());
+	  });
+  //  connect(ui->buttonBox, ...
+}
+  
+BoardSizeDialog::~BoardSizeDialog() {
+  delete ui;
+}
+
+void BoardSizeDialog::setLayout(class Layout const &lay) {
+  minrect = lay.root().boundingRect();
+  ui->width->setValue(lay.board().width);
+  ui->height->setValue(lay.board().height);
+}
+
+Dim BoardSizeDialog::boardWidth() const {
+  return ui->width->value();
+}
+
+Dim BoardSizeDialog::boardHeight() const {
+  return ui->height->value();
+}
+
