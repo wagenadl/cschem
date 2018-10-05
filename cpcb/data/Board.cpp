606996c3ca2a2c3fb69e80e22a47b5529c97c34e
Daniel A. Wagenaar <daw@caltech.edu>
2018-10-05 12:56:55 -0700

    cpcb: <board> in xml split up into <outline>, <options>, <links> sections

--- cpcb/data/Board.cpp
+++ cpcb/data/Board.cpp
@@ -16,38 +16,79 @@ Board::Board() {
 
 QXmlStreamWriter &operator<<(QXmlStreamWriter &s, Board const &t) {
   s.writeStartElement("board");
+  s.writeStartElement("outline");
+  s.writeAttribute("shape", "rect");
   s.writeAttribute("w", t.width.toString());
   s.writeAttribute("h", t.height.toString());
+  s.writeEndElement(); // outline
+  s.writeStartElement("options");
   s.writeAttribute("grid", t.grid.toString());
   s.writeAttribute("metric", t.metric ? "1" : "0");
   s.writeAttribute("planesvis", t.planesvisible ? "1" : "0");
   for (Layer l: t.layervisible.keys())
     s.writeAttribute(QString("layer%1vis").arg(int(l)),
 		     t.layervisible[l] ? "1" : "0");
+  s.writeEndElement(); // options
+  s.writeStartElement("links");
   if (!t.linkedschematic.isEmpty())
     s.writeAttribute("schem", t.linkedschematic);
-  s.writeEndElement();
+  s.writeEndElement(); // links
+  s.writeEndElement(); // board
   return s;
 }
-  
+
+static void readOutline(QXmlStreamReader &s, Board &t) {
+  auto a = s.attributes();
+  t.width = Dim::fromString(a.value("w").toString());
+  t.height = Dim::fromString(a.value("h").toString());
+}
+
+static void readOptions(QXmlStreamReader &s, Board &t) {
+  auto a = s.attributes();
+  t.grid = Dim::fromString(a.value("grid").toString());
+  t.metric = a.value("metric").toInt();
+  t.planesvisible = a.value("planesvis").toInt();
+  for (Layer l: t.layervisible.keys())
+    t.layervisible[l] = a.value(QString("layer%1vis").arg(int(l))).toInt();
+}
+
+static void readLinks(QXmlStreamReader &s, Board &t) {
+  auto a = s.attributes();
+  t.linkedschematic = a.value("schem").toString();
+}  
+
 QXmlStreamReader &operator>>(QXmlStreamReader &s, Board &t) {
   t = Board();
   bool ok;
-  auto a = s.attributes();
-  t.width = Dim::fromString(a.value("w").toString(), &ok);
-  if (ok)
-    t.height = Dim::fromString(a.value("h").toString(), &ok);
-  if (ok)
-    t.grid = Dim::fromString(a.value("grid").toString(), &ok);
-  if (ok)
-    t.metric = a.value("metric").toInt(&ok);
-  if (ok)
-    t.planesvisible = a.value("planesvis").toInt(&ok);
-  for (Layer l: t.layervisible.keys())
-    if (ok)
-      t.layervisible[l] = a.value(QString("layer%1vis").arg(int(l))).toInt(&ok);
-  if (ok)
-    t.linkedschematic = a.value("schem").toString();
+  if (s.attributes().hasAttribute("w")) {
+    // old style: all-in-one
+    readOutline(s, t);
+    readOptions(s, t);
+    readLinks(s, t);
+  } else {
+    // new style - with subelements <outline>, <options>, and <links>
+    while (!s.atEnd()) {
+      s.readNext();
+      if (s.isStartElement()) {
+	qDebug() << "got start" << s.name();
+	if (s.name()=="outline")
+	  readOutline(s, t);
+	else if (s.name()=="options")
+	  readOptions(s, t);
+	else if (s.name()=="links")
+	  readLinks(s, t);
+	else
+	  qDebug() << "Unexpected element in <board>: " << s.name();
+	s.skipCurrentElement();
+      } else if (s.isEndElement()) {
+	return s;
+      } else if (s.isCharacters() && s.isWhitespace()) {
+      } else if (s.isComment()) {
+      } else {
+	qDebug() << "Unexpected entity in <board>" << s.tokenType();
+      }
+    }
+  }
   s.skipCurrentElement();
   return s;
 }
