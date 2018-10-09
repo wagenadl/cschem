e1a0428ba416a00d4da909ad228e196ea6011ea2
Daniel A. Wagenaar <daw@caltech.edu>
2018-10-09 11:02:32 -0700

    cschem annotations now typeset math very nicely

--- cschem/ui/SceneTextual.cpp
+++ cschem/ui/SceneTextual.cpp
@@ -11,6 +11,8 @@
 #include <QTextBlock>
 #include <QTextLine>
 #include <QGraphicsSceneMouseEvent>
+#include <QRegularExpression>
+#include <QTextCursor>
 
 class STData {
 public:
@@ -39,8 +41,97 @@ void STData::rebuild() {
   rebuildPos();
 }
 
+static QString rebuildLine(QString str) {
+  QStringList bits = str.split(" ", QString::KeepEmptyParts);
+
+  QRegularExpression vari("^([A-Z]+)([.0-9]*[0-9])([.,?!:;]?)$");
+  QRegularExpression oper("^(=|\\+|\\-|\\<|\\>|≤|≥|≠|±|≈)$");
+  QRegularExpression voltcurrent("^(V|I)([A-Za-z0-9]*)(.[0-9]+)?([.,?!:;]?)$");
+  QRegularExpression name("^([A-Za-z])");
+  QRegularExpression periodnum("\\.([0-9])");
+  QRegularExpression ending("[.,?!:;]$");
+  QRegularExpression singleletter("^[B-HJ-UW-Z][.,?!:;]?$");
+
+  auto isMath = [=](int n) {
+    if (n>=0 && n<bits.size()) {
+      QString bit = bits[n];
+      if (oper.match(bit).hasMatch())
+	return true;
+      else if (vari.match(bit).hasMatch())
+	return true;
+    }
+    return false;
+  };
+
+  auto impliedMath = [=](int n) {
+    if (n>=0 && n<bits.size()) {
+      if (isMath(n) || singleletter.match(bits[n]).hasMatch())
+	return true;
+      if ((!isMath(n-1) || (n>0 && ending.match(bits[n-1]).hasMatch()))
+	   && !isMath(n+1))
+	return false;
+      QString bit = bits[n];
+      if (voltcurrent.match(bit).hasMatch())
+	return true;
+    }
+  };
+
+  auto markup = [=](QString str) {
+    auto mtch = voltcurrent.match(str);
+    if (mtch.hasMatch()) {
+      QString pfx = mtch.captured(1);
+      QString ifx = mtch.captured(2) + mtch.captured(3);
+      QString sfx = mtch.captured(4);
+      if (ifx>="A") {
+	ifx.replace("0", "₀");
+	ifx.replace("1", "₁");
+	ifx.replace("2", "₂");
+	ifx.replace("3", "₃");
+	ifx.replace("4", "₄");
+	ifx.replace("5", "₅");
+	ifx.replace("6", "₆");
+	ifx.replace("7", "₇");
+	ifx.replace("8", "₈");
+	ifx.replace("9", "₉");
+	ifx.replace(periodnum, "܂" 
+		    "\\1");
+      }
+      return "<i>" + pfx + "</i><sub>" + ifx + "</sub>" + sfx;
+    } else {
+      auto mtch = vari.match(str);
+      if (mtch.hasMatch())
+	return "<i>" + mtch.captured(1) + "</i><sub>"
+	  + mtch.captured(2) + "</sub>" + mtch.captured(3);
+      else if (name.match(str).hasMatch())
+	return "<i>" + str + "</i>";
+    }
+    return str;
+  };
+    
+  for (int n=0; n<bits.size(); n++)
+    if (impliedMath(n))
+      bits[n] = markup(bits[n]);
+
+  return bits.join("&nbsp;");
+}
+  
+
 void STData::rebuildText() {
-  st->document()->setPlainText(txt.text);
+  txt.text.replace("<=", "≤");
+  txt.text.replace(">=", "≥");
+  txt.text.replace("!=", "≠");
+  txt.text.replace("+-", "±");
+  txt.text.replace("~~", "≈");
+  txt.text.replace("Ohm", "Ω");
+  QStringList lines = txt.text.split("\n");
+  for (int n=0; n<lines.size(); n++)
+    lines[n] = rebuildLine(lines[n]);
+  
+  QTextCursor c(st->textCursor());
+  int pos = c.position();
+  st->document()->setHtml(lines.join("<br>"));
+  c.setPosition(pos);
+  st->setTextCursor(c);
 }
 
 void STData::rebuildPos() {
@@ -59,7 +150,6 @@ void STData::rebuildPos() {
 
 SceneTextual::SceneTextual(class Scene *parent, class Textual const &txt):
   d(new STData(this, parent, txt)) {
-  qDebug() << "SceneTextual" << txt;
   setFont(Style::annotationFont());
   setTextInteractionFlags(Qt::TextEditorInteraction);
   setFlags(ItemIsFocusable);
@@ -67,9 +157,14 @@ SceneTextual::SceneTextual(class Scene *parent, class Textual const &txt):
   d->rebuild();
   d->scene->addItem(this);
   connect(document(), &QTextDocument::contentsChanged,
-	  [this]() { QString t = document()->toPlainText();
+	  [this]() {
+	    QString t = document()->toPlainText();
+	    if (t==d->txt.text)
+	      return;
 	    d->txt.text = t;
-	    d->scene->storeTextualText(id(), t); });
+	    d->rebuildText();
+	    d->scene->storeTextualText(id(), d->txt.text);
+	  });
 }
   
 SceneTextual::~SceneTextual() {
