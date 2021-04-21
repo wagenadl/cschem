b100ad8365b072d5a4a441f60f1da72cd27ae44c
Daniel A. Wagenaar <daw@caltech.edu>
2021-04-20 18:55:10 -0700

    No longer relies on soft links

--- cpcb/ui/Tracer.h
+++ cpcb/ui/Tracer.h
@@ -5,20 +5,21 @@
 #define TRACER_H
 
 #include "data/Layer.h"
+#include "data/Point.h"
 
 class Tracer {
 public:
   Tracer(class EData *);
   ~Tracer();
-  void start(class Point const &); // start at exactly this point
-  void pickup(class Point const &);
-  void click(class Point const &);
-  void move(class Point const &);
+  void start(Point const &); // start at exactly this point
+  void pickup(Point const &);
+  void click(Point const &);
+  void move(Point const &);
   void confirm();
   void render(class QPainter &);
   void end();
   void setLayer(Layer const &);
-  void setLinewidth(class Dim const &);
+  void setLinewidth(Dim const &);
   bool isTracing() const;
 private:
   class TRData *d;
