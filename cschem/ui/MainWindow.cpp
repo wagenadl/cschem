1f4c83720b86be8319f9878204f47aee20f34fc5
Daniel A. Wagenaar <daw@caltech.edu>
2022-03-15 16:15:22 -0700

    Pushed version to 0.2.0. Now uses CMake. Windows/Mac build not yet ported.

--- cschem/ui/MainWindow.cpp
+++ cschem/ui/MainWindow.cpp
@@ -450,7 +450,7 @@ void MainWindow::aboutAction() {
   QString vsn = Style::versionName();
   QMessageBox::about(0, "About " + me,
                      me + " " + vsn
-                     + "<p>" + "(C) 2018–2019 Daniel A. Wagenaar\n"
+                     + "<p>" + "(C) 2018–2022 Daniel A. Wagenaar\n"
                      + "<p>" + me + " is a program for electronic circuit layout with high-quality SVG export. More information is available at <a href=\"http://www.danielwagenaar.net/cschem\">www.danielwagenaar.net/cschem</a>.\n"
                      + "<p>" + me + " is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n"
                      + "<p>" + me + " is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\n"
