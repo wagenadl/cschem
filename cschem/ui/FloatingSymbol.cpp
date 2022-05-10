e8b3c1a53f3d6ac01af1d174d917c5dbc65cea01
Daniel A. Wagenaar <daw@caltech.edu>
2022-05-10 15:50:31 -0700

    Bugfix to drag-in of symbols.

--- cschem/ui/FloatingSymbol.cpp
+++ cschem/ui/FloatingSymbol.cpp
@@ -5,25 +5,31 @@
 
 class FloatingSymbolData {
 public:
-  FloatingSymbolData() {
+  FloatingSymbolData(Symbol const &sym1): sym(sym1) {
+  bbOrigin = sym.bbOrigin();
+  sCM = sym.shiftedBBox().center();
+  name = sym.name();
   }
 public:
+  Symbol sym;
   QPointF bbOrigin;
   QPointF sCM;
   QString name;
 };
 
 FloatingSymbol::FloatingSymbol(Symbol const &symbol):
-  d(new FloatingSymbolData()) {
-  d->bbOrigin = symbol.bbOrigin();
-  d->sCM = symbol.shiftedBBox().center();
-  d->name = symbol.name();
-  setRenderer(symbol.renderer());
+  d(new FloatingSymbolData(symbol)) {
+  setRenderer(d->sym.renderer());
 }
   
 FloatingSymbol::~FloatingSymbol() {
   delete d;
 }
+
+Symbol const &FloatingSymbol::symbol() const {
+  return d->sym;
+}
+      
 
 void FloatingSymbol::setSymbolPosition(QPointF p) {
   setPos(p - d->bbOrigin);
--- cschem/ui/FloatingSymbol.h
+++ cschem/ui/FloatingSymbol.h
@@ -13,6 +13,7 @@ public:
   FloatingSymbol(FloatingSymbol const &) = delete;
   FloatingSymbol &operator=(FloatingSymbol const &) = delete;
   void setSymbolPosition(QPointF); // set position of first pin
+  Symbol const &symbol() const;
   QPointF symbolPosition() const;
   QPointF shiftedCenter() const; // position of center relative to first pin
   QString name() const;
