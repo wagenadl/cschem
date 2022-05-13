e8e21f0e38c98510e7f8a3d997f42fe29ea81d6e
Daniel A. Wagenaar <daw@caltech.edu>
2022-05-13 09:17:11 -0700

    Better error reporting for invalid symbol files.

--- cschem/ui/Scene.cpp
+++ cschem/ui/Scene.cpp
@@ -18,6 +18,7 @@
 #include "PartList.h"
 #include "circuit/PartNumbering.h"
 #include "SceneTextual.h"
+#include <QMessageBox>
 
 class SceneData {
 public:
@@ -1017,8 +1018,8 @@ void SceneData::hideDragIn() {
 bool SceneData::startSvgDragIn(QString filename, QPointF pos) {
   Symbol symbol = Symbol::load(filename);
   qDebug() << "startSvgDragIn" << filename << pos << symbol.isValid();
-  if (!symbol.isValid())
-    return false;
+  //  if (!symbol.isValid())
+  //return false;
 
   if (dragin)
     delete dragin;
@@ -1159,11 +1160,20 @@ void Scene::dropEvent(QGraphicsSceneDragDropEvent *e) {
       return;
     }
     Symbol const &sym = d->dragin->symbol();
-    if (d->importAndPlonk(sym, droppos, true)) {
-      emit libraryChanged();
-      e->accept();
-      d->hideDragIn();
-      return;
+    if (sym.isValid()) {
+      if (d->importAndPlonk(sym, droppos, true)) {
+        emit libraryChanged();
+        e->accept();
+        d->hideDragIn();
+        return;
+      } else {
+        QMessageBox::warning(0, "CSchem",
+                             "Failed to import symbol file. Reason unknown.");
+      }
+    } else {
+      QMessageBox::warning(0, "CSchem",
+                           "Failed to import symbol file:\n\n"
+                           + sym.problems().join("\n"));
     }
   }
 
