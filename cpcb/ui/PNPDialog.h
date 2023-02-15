4e62d54de81680efe0c27a1f5f76f103c17570e3
Daniel A. Wagenaar <daw@caltech.edu>
2023-02-15 14:30:02 -0800

    Work on PNP Dialog

--- cpcb/ui/PNPDialog.h
+++ cpcb/ui/PNPDialog.h
@@ -7,18 +7,25 @@
 #include <QDialog>
 
 class PNPDialog: public QDialog {
-  
+  Q_OBJECT;
 public:
   PNPDialog(QWidget *parent=0);
   virtual ~PNPDialog();
+  void setFolder(QString pwd);
+  void setPNPFilename(QString fn);
   bool bomChecked() const;
   bool compactChecked() const;
+  bool unplacedChecked() const;
   bool imageChecked() const;
-  QString filename() const;
+  QString pnpFilename() const;
+  QString unplacedFilename() const;
+  QString bomFilename() const;
+  QString imageFilename() const;
 public slots:
   void browse();
 private:
   class Ui_PNPDialog *ui;
+  QString pwd;
 };
 
 #endif
