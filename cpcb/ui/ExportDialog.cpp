f39a4cc4c8e3c015cf8f6b2ceb9e601e18c4be97
Daniel A. Wagenaar <daw@caltech.edu>
2023-02-17 13:02:40 -0800

    Working on PNP

--- cpcb/ui/ExportDialog.cpp
+++ cpcb/ui/ExportDialog.cpp
@@ -21,7 +21,7 @@ bool ExportDialog::runDialog(QString pcbfilename, QString exportdir) {
     ui->gerberfilename->setText("");
   else
     ui->gerberfilename->setText(fi.absolutePath()
-                                + "/" + fi.baseName() + ".zip");
+                                + "/" + fi.completeBaseName() + ".zip");
   return exec();
 }
 
@@ -50,7 +50,7 @@ void ExportDialog::gerbernamechange() {
     ui->unplacedfilename->setText("");
     ui->imagefilename->setText("");
   } else {
-    QString base = fn.baseName();
+    QString base = fn.completeBaseName();
     ui->bomfilename->setText(".../" + base + "-bom.csv");
     ui->pnpfilename->setText(".../" + base + "-pnp.csv");
     ui->unplacedfilename->setText(".../" + base + "unplaced.csv");
@@ -60,4 +60,56 @@ void ExportDialog::gerbernamechange() {
 
 bool ExportDialog::saveAccordingly(class Layout const &pcblayout,
                                    class LinkedSchematic const &schematic) {
+  QString gerberfn = ui->gerberfilename->getText();
+  if (gerberfn.isEmpty())
+    return false;
+  if (!saveGerber(pcblayout))
+    return false;
+  if (!savePnP(pcblayout, schematic))
+    return false;
+}
+
+bool ExportDialog::saveGerber(class Layout const &pcblayout) {
+  QFileInfo fi(ui->gerberfilename->getText());
+  QString fn = fi.absoluteFilePath();
+  QString base = fi.completeBaseName();
+  QTemporaryDir td;
+  bool ok = false;
+  if (td.isValid()) {
+    if (GerberWriter::write(editor->pcbLayout(), td.filePath(base))) {
+      QDir cwd = QDir::current();
+      QDir::setCurrent(td.path());
+      QStringList args;
+      args << "-r" << fn << base;
+      ok = QDir::root().remove(fn)
+        && (QProcess::execute("zip", args)==0);
+      QDir::setCurrent(cwd.absolutePath());
+    }
+  }
+  QDir(td.filePath(base)).removeRecursively();
+  if (!ok)
+    QMessageBox::warning(parent(), "cpcb",
+                         "Could not export pcb as “"
+                         + fn + "”",
+                         QMessageBox::Ok);
+  return ok;
+}
+
+bool ExportDialog::savePNP(class Layout const &pcblayout,
+                           class LinkedSchematic const &schematic) ) {
+  QFileInfo fi(ui->gerberfilename->getText());
+  QString fn = fi.absoluteFilePath();
+  QString base = fi.completeBaseName();
+  QString pnpfn = dir + "/" + base + "-pnp.csv";
+
+  PickNPlace pnp(pcblayout.root(),
+                 ui->smtonly->isChecked()
+                 ? PickAndPlace::Scope::SMTOnly
+                 : PickAndPlace::Scope::SMTAndThruHole);
+  
+  QString bomfn = dir + "/" + base + "-bom.csv";
+  QString unplacedfn = dir + "/" + base + "-unplaced.csv";
+  QString imagefn = dir + "/" + base + "-pngimage.png";
+
+  
 }
