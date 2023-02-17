bbbad2b738e277b77f1da3a48b1cfb33eeb45b08
Daniel A. Wagenaar <daw@caltech.edu>
2023-02-17 13:02:48 -0800

    Working on PNP

--- cpcb/ui/ExportDialog.h
+++ cpcb/ui/ExportDialog.h
@@ -12,12 +12,14 @@ public:
   ExportDialog(QWidget *parent=0);
   virtual ~ExportDialog();
   bool runDialog(QString pcbfilename, QString exportdir);
-  void saveAccordingly(class Layout const &pcblayout,
+  bool saveAccordingly(class Layout const &pcblayout,
                        class LinkedSchematic const &schematic);
   QString filename() const;
 private slots:
   void browse();
   void gerbernamechange();
+private:
+  bool saveGerber(class Layout const &pcblayout);
 private:
   class Ui_PNPDialog *ui;
   QString pwd;
